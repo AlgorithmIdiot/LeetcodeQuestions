706036294f43a4b76e204513010d5854eee4e5f2
cwangbh <cwangbh@connect.ust.hk>
2020-10-26 19:00:03 +0800

    Union_find

---
+++ 玩儿转算法面试/Union_Find/UnionFind3.h
@@ -0,0 +1,63 @@
+#pragma once
+#include<cassert>
+
+namespace UF3 {
+	class UnionFind {
+	private:
+		int* parent;
+		int* rank; // sz[i] 表示以i 为根的
+		int count;
+	public:
+		UnionFind(int count) {
+			parent = new int[count];
+			rank = new int[count];// 以i为根的集合的层数
+			this->count = count;
+			for (int i = 0; i < count; i++)
+			{
+				parent[i] = i;
+				rank[i] = 1;
+			}
+				
+		}
+		~UnionFind()
+		{
+			delete[] parent;
+			delete[] rank;
+		}
+
+		int find(int p)
+		{
+			assert(p >= 0 && p < count);
+			while (p != parent[p])
+				p = parent[p];
+			return p;
+		}
+
+		bool isConnected(int p, int q)
+		{
+			return find(p) == find(q);
+		}
+
+		void unionElements(int p, int q) {
+			int pRoot = find(p);
+			int qRoot = find(q);
+			if (pRoot == qRoot)
+				return;
+			// 由短的列表指向长的列表
+			if (rank[pRoot] < rank[qRoot]) {
+				parent[pRoot] = parent[qRoot];
+				//rank[qRoot] += rank[pRoot];
+			}
+			else if (rank[qRoot] < rank[pRoot])
+			{
+				parent[qRoot] = parent[pRoot];
+			}
+			else
+			{//rank[qRoot] = rank[pRoot]
+				parent[pRoot] = qRoot;
+				rank[qRoot] += 1;
+			}
+			
+		}
+	};
+}
---
+++ 玩儿转算法面试/Union_Find/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"UnionFindTestHelpter.h"
+using namespace std;
+
+int main() {
+	int n = 1000;
+	//UnionFindTestHelper::testUF1(n);
+	//UnionFindTestHelper::testUF2(n);
+	UnionFindTestHelper::testUF3(n);
+	UnionFindTestHelper::testUF4(n);
+	cin.get();
+	return 0;
+}
